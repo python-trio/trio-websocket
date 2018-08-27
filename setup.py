530fb094f31a930aa36caa899c0e7462a566c09f
Mark E. Haase <mehaase@gmail.com>
2018-08-27 13:45:47 -0400

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='trio-websocket',
+    version='0.0.1',
+    description='WebSocket library for Trio',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mehaase/trio-websocket',
+    author='Mark E. Haase',
+    author_email='mehaase@gmail.com',
+    classifiers=[
+        # See https://pypi.org/classifiers/
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='websocket client server trio',
+    packages=find_packages(exclude=['docs', 'examples', 'tests']),
+    install_requires=['trio', 'trustme', 'wsproto'],
+    extras_require={
+        'wsaccel': ['wsaccel'],
+    },
+    project_urls={
+        'Bug Reports': 'https://github.com/mehaase/trio-websocket/issues',
+        'Source': 'https://github.com/mehaase/trio-websocket',
+    },
+)
