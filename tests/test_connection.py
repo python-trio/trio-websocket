python-trio/trio-websocket

commit 0233e2176731a190a515c025c0762c19fbde7821

Remove yarl dependency; replace with urllib.parse

trio_websocket/_impl.py
@@ -6,6 +6,7 @@ import logging
 import random
 import ssl
 import struct
+import urllib.parse
 
 from async_generator import async_generator, yield_, asynccontextmanager
 import trio
@@ -25,7 +26,6 @@ from wsproto.events import (
     TextMessage,
 )
 import wsproto.utilities
-from yarl import URL
 
 
 CONN_TIMEOUT = 60 # default connect & disconnect timeout, in seconds
@@ -247,14 +247,23 @@ def _url_to_host(url, ssl_context):
     :type ssl_context: ssl.SSLContext or None
     :returns: A tuple of ``(host, port, resource, ssl_context)``.
     '''
-    url = URL(url)
-    if url.scheme not in ('ws', 'wss'):
+    url = str(url)  # For backward compat with isinstance(url, yarl.URL).
+    parts = urllib.parse.urlsplit(url)
+    if parts.scheme not in ('ws', 'wss'):
         raise ValueError('WebSocket URL scheme must be "ws:" or "wss:"')
     if ssl_context is None:
-        ssl_context = url.scheme == 'wss'
-    elif url.scheme == 'ws':
+        ssl_context = parts.scheme == 'wss'
+    elif parts.scheme == 'ws':
         raise ValueError('SSL context must be None for ws: URL scheme')
-    return url.host, url.port, url.path_qs, ssl_context
+    host = parts.hostname
+    if parts.port is not None:
+        port = parts.port
+    else:
+        port = 443 if ssl_context else 80
+    path_qs = parts.path
+    if '?' in url:
+        path_qs += '?' + parts.query
+    return host, port, path_qs, ssl_context
 
 
 async def wrap_client_stream(nursery, stream, host, resource, *,
@@ -1328,7 +1337,7 @@ class WebSocketServer:
         Start serving incoming connections requests.
 
         This method supports the Trio nursery start protocol: ``server = await
-        nursery.start(server.run, …)``. It will block until the server is 
+        nursery.start(server.run, …)``. It will block until the server is
         accepting connections and then return a :class:`WebSocketServer` object.
 
         :param task_status: Part of the Trio nursery start protocol.

tests/test_connection.py
@@ -287,6 +287,13 @@ async def test_client_open_invalid_url(echo_server):
             pass
 
 
+async def test_ascii_encoded_path_is_ok(echo_server):
+    path = '%D7%90%D7%91%D7%90?%D7%90%D7%9E%D7%90'
+    url = 'ws://{}:{}{}/{}'.format(HOST, echo_server.port, RESOURCE, path)
+    async with open_websocket_url(url) as conn:
+        assert conn.path == RESOURCE + '/' + path
+
+
 @patch('trio_websocket._impl.open_websocket')
 def test_client_open_url_options(open_websocket_mock):
     """open_websocket_url() must pass its options on to open_websocket()"""

autobahn/client.py
@@ -7,7 +7,6 @@ import logging
 
 import trio
 from trio_websocket import open_websocket_url, ConnectionClosed
-from yarl import URL
 
 
 AGENT = 'trio-websocket'
@@ -16,7 +15,7 @@ logger = logging.getLogger('client')
 
 
 async def get_case_count(url):
-    url = URL(url).with_path('/getCaseCount')
+    url = url + '/getCaseCount'
     async with open_websocket_url(url) as conn:
         case_count = await conn.get_message()
         logger.info('Case count=%s', case_count)
@@ -24,7 +23,7 @@ async def get_case_count(url):
 
 
 async def run_case(url, case):
-    url = URL(url).with_path('/runCase').with_query(case=case, agent=AGENT)
+    url = url + '/runCase?case={}&agent={}'.format(case, AGENT)
     try:
         async with open_websocket_url(url) as conn:
             while True:
@@ -35,7 +34,7 @@ async def run_case(url, case):
 
 
 async def update_reports(url):
-    url = URL(url).with_path('/updateReports').with_query(agent=AGENT)
+    url = url + '/updateReports?agent={}'.format(AGENT)
     async with open_websocket_url(url) as conn:
         # This command runs as soon as we connect to it, so we don't need to
         # send any messages.

docs/conf.py
@@ -177,5 +177,4 @@ epub_exclude_files = ['search.html']
 # -- Extension configuration -------------------------------------------------
 intersphinx_mapping = {
     'trio': ('https://trio.readthedocs.io/en/stable/', None),
-    'yarl': ('https://yarl.readthedocs.io/en/stable/', None),
 }

examples/client.py
@@ -10,10 +10,10 @@ import logging
 import pathlib
 import ssl
 import sys
+import urllib.parse
 
 import trio
 from trio_websocket import open_websocket_url, ConnectionClosed, HandshakeError
-import yarl
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -40,7 +40,7 @@ def parse_args():
 
 async def main(args):
     ''' Main entry point, returning False in the case of logged error. '''
-    if yarl.URL(args.url).scheme == 'wss':
+    if urllib.parse.urlsplit(args.url).scheme == 'wss':
         # Configure SSL context to handle our self-signed certificate. Most
         # clients won't need to do this.
         try:

setup.py
@@ -42,7 +42,6 @@ setup(
         'async_generator>=1.10,<2',
         'trio>=0.11',
         'wsproto>=0.14,<0.16',
-        'yarl>=1.2.6,<2'
     ],
     project_urls={
         'Bug Reports': 'https://github.com/HyperionGray/trio-websocket/issues',

commit 314d7a6217931fd1464f9d84a8c84d71699a0c32

Fix " Illegal target characters" when connecting to URL with zero-length path
This regressed in 0233e2176731a190a515c025c0762c19f (version 0.9.0) due
to different behavior between yarl and urllib.parse.urlsplit on a URL
without a path component like "wss://ws.kraken.com". yarl makes this `/`
while urlsplit makes it ''. h11 expects `/` as specified in the quoted
RFC.

trio_websocket/_impl.py
@@ -291,6 +291,11 @@ def _url_to_host(url, ssl_context):
     else:
         port = 443 if ssl_context else 80
     path_qs = parts.path
+    # RFC 7230, Section 5.3.1:
+    # If the target URI's path component is empty, the client MUST
+    # send "/" as the path within the origin-form of request-target.
+    if not path_qs:
+        path_qs = '/'
     if '?' in url:
         path_qs += '?' + parts.query
     return host, port, path_qs, ssl_context

tests/test_connection.py
@@ -276,6 +276,10 @@ async def test_client_open(echo_server):
 
 
 async def test_client_open_url(echo_server):
+    url = 'ws://{}:{}/'.format(HOST, echo_server.port)
+    async with open_websocket_url(url) as conn:
+        assert conn.path == '/'
+
     url = 'ws://{}:{}{}/path'.format(HOST, echo_server.port, RESOURCE)
     async with open_websocket_url(url) as conn:
         assert conn.path == RESOURCE + '/path'
@@ -283,6 +287,11 @@ async def test_client_open_url(echo_server):
     url = 'ws://{}:{}{}?foo=bar'.format(HOST, echo_server.port, RESOURCE)
     async with open_websocket_url(url) as conn:
         assert conn.path == RESOURCE + '?foo=bar'
+
+    # Zero-length path becomes '/'.
+    url = 'ws://{}:{}'.format(HOST, echo_server.port)
+    async with open_websocket_url(url) as conn:
+        assert conn.path == '/'
 
 
 async def test_client_open_invalid_url(echo_server):