bebd9cf29faafd7c71d7bdedb231f673b9d24917
John Belmonte <john@neggie.net>
2023-03-11 23:59:24 +0900

    drop Python 3.6 support

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
@@ -37,7 +36,7 @@ setup(
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     keywords='websocket client server trio',
     packages=find_packages(exclude=['docs', 'examples', 'tests']),
     install_requires=[
